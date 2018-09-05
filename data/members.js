a25c1dea105b94d11145ef5475c4580743c5c441
yuvalbl <yuval.bar.levi@gmail.com>
2018-09-05 18:46:50 +0300

    update members

--- data/members.js
+++ data/members.js
@@ -3,7 +3,7 @@ const members = [
   {name: 'yuval', id: 111},
   {name: 'roy', id: 342},
   {name: 'furman', id: 981},
-  {name: 'chaim', id: 111}
+  {name: 'chaim', id: 191}
 ];
 
 module.exports = members;
